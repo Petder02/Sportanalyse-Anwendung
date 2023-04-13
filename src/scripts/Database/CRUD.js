ef6f63eb9cf652fcc4bfadf070cc6b643a4c68a8
Petder02 <pjs182@case.edu>
2023-04-13 11:54:49 -0400

    Created and tested batch player data updates
    Player data is now all within the database. That should conclude necessary database work for the football component of the app unless the API key expires

--- src/scripts/Database/CRUD.js
+++ src/scripts/Database/CRUD.js
@@ -2,8 +2,9 @@
  * Posts player data from the Sports.io API into a MongoDB
  * @param season
  * @param team
+ * @param timeoutDelay Specified how long each call should be delayed by
  */
-async function postPlayerData(season, team) {
+async function postPlayerData(season, team, timeoutDelay=1000) {
     const fdClientModule = require('fantasydata-node-client');
     const keys = {
         'NFLv3StatsClient': '9a89010dda0643388baf8867abb798df',
@@ -14,7 +15,6 @@ async function postPlayerData(season, team) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const teamsCollection = dbConnection.collection('players');
         FantasyDataClient.NFLv3StatsClient.getPlayerSeasonStatsByTeamPromise(season, team)
             .then((resp) => {
                 //You must work with the response (resp) in this callback function. It cannot be used outside the function.
@@ -22,8 +22,13 @@ async function postPlayerData(season, team) {
                 for (const player of players) {
                     removeUnneededPlayerData(player);
                 }
-                console.log(players[0]);
-                players.forEach(player => postPlayerToMongoDB(player, dbConnection));
+                players.forEach((player, index) => {
+                    setTimeout((() => {
+                        postPlayerToMongoDB(player, dbConnection)
+                        console.log("Team -> " + team + " | Season -> " + season + " | Inserted player " + (index + 1) + "/" + players.length);
+                    }), (index + 1) * timeoutDelay)
+                });
+                console.log("Finished inserting players from " + team + " in " + season + "season");
             })
             .catch((err) => {
                 console.error("An error has occurred -> " + err);
@@ -31,17 +36,21 @@ async function postPlayerData(season, team) {
     }).catch((err) => "An error has occurred -> " + err);
 }
 
-//Posts a player to mongo db
-//Player must be a singular object, db must be the database instance
+/**
+ * Posts a player to mongo db
+ * @param player
+ * @param db
+ */
 function postPlayerToMongoDB(player, db) {
     db.collection('players')
         .replaceOne(player, player, {upsert : true})
         .then(result => {
-            console.log("Successfully inserted player");
+            console.log("Successfully inserted player with name -> " + player["Name"]);
         })
         .catch(err => {
-            console.error('Error inserting player: ', err);
+            console.error(`Error inserting player with name -> ${player["Name"]}: `, err);
         });
+    console.log("Finishing");
 }
 
 /**
@@ -59,9 +68,9 @@ async function getPlayerDataByNameAndSeason(playerName, season) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const testCollection = dbConnection.collection('players');
+        const playerCollection = dbConnection.collection('players');
         //Find data by team and season
-        testCollection.find({
+        playerCollection.find({
             Name : playerName,
             Season : season
         }).toArray().then((result) => {
@@ -88,9 +97,9 @@ async function getPlayerDataByPlayerIDAndSeason(playerID, season) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const testCollection = dbConnection.collection('players');
+        const playerCollection = dbConnection.collection('players');
         //Find data by team and season
-        testCollection.find({
+        playerCollection.find({
             PlayerID : playerID,
             Season : season
         }).toArray().then((result) => {
@@ -107,8 +116,9 @@ async function getPlayerDataByPlayerIDAndSeason(playerID, season) {
 /**
  * Posts all team data from the Sports.io API from a particular season to a MongoDB
  * @param season
+ * @param timeoutDelay the amount of time to delay each call by
  */
-async function postTeamData(season) {
+async function postTeamData(season, timeoutDelay=1000) {
     const fdClientModule = require('fantasydata-node-client');
     const keys = {
         'NFLv3StatsClient': '9a89010dda0643388baf8867abb798df',
@@ -119,12 +129,16 @@ async function postTeamData(season) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const teamsCollection = dbConnection.collection('teams');
         FantasyDataClient.NFLv3StatsClient.getTeamSeasonStatsPromise(season)
             .then((resp) => {
                 //You must work with the response (resp) in this callback function. It cannot be used outside the function.
                 let teams = JSON.parse(resp); // <- This is an array of JSON instances. Each instance is a team with their statistics from a given season (see season parameter). There are 32 items in this array cause there are 32 NFL teams.
-                teams.forEach(team => postTeamToMongoDB(team, dbConnection));
+                teams.forEach((team, index) => {
+                    setTimeout((() => {
+                        postTeamToMongoDB(team, dbConnection)
+                        console.log("Inserted team " + (index + 1) + "/" + teams.length);
+                    }), (index + 1) * timeoutDelay)
+                });
             })
             .catch((err) => {
                 console.error("An error has occurred -> " + err)
@@ -132,16 +146,19 @@ async function postTeamData(season) {
     }).catch((err) => "An error has occurred -> " + err);
 }
 
-//Posts a team to mongo db
-//Player must be a singular object, db must be the database instance
+/**
+ * Posts a team to mongo db
+ * @param team
+ * @param db
+ */
 function postTeamToMongoDB(team, db) {
     db.collection('teams')
         .replaceOne(team, team, {upsert : true})
-        .then(result => {
-            console.log("Successfully inserted team");
+        .then(() => {
+            console.log("Successfully inserted team with abbreviation -> " + team);
         })
         .catch(err => {
-            console.error('Error inserting team: ', err);
+            console.error('Error inserting team with abbreviation -> ' + team, err);
         });
 }
 
@@ -159,9 +176,9 @@ async function getTeamDataByNameAndSeason(teamName, season) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const testCollection = dbConnection.collection('teams');
+        const teamCollection = dbConnection.collection('teams');
         //Find data by team and season
-        testCollection.find({
+        teamCollection.find({
             Team : teamName,
             Season : season
         }).toArray().then((result) => {
@@ -188,9 +205,9 @@ async function getTeamDataByTeamIDAndSeason(teamID, season) {
 
     MongoClient.connect(uri).then((client) => {
         const dbConnection = client.db('american_football');
-        const testCollection = dbConnection.collection('teams');
+        const teamCollection = dbConnection.collection('teams');
         //Find data by team and season
-        testCollection.find({
+        teamCollection.find({
             TeamID : teamID,
             Season : season
         }).toArray().then((result) => {
@@ -207,39 +224,37 @@ async function getTeamDataByTeamIDAndSeason(teamID, season) {
  * Method which updates team data from a starting season to an ending season in a batch (range -> [startSeason, endSeason])
  * @param startSeason
  * @param endSeason
+ * @param timeDelay the amount of time to delay each call by
+ *                  if only updating one season, this can be set to ~100. For every additional season, multiply the time out by between 2-2.5x the previous value
  * @returns {Promise<void>}
  */
-async function batchUpdateTeamData(startSeason, endSeason) {
+async function batchUpdateTeamData(startSeason, endSeason, timeDelay=1000) {
     let seasons = [];
     let currSeason = startSeason;
     for (let i = startSeason; i <= endSeason; i++) {
         seasons.push(currSeason++);
     }
     seasons.forEach((season) => {
-        postTeamData(`${season}REG`);
+        setTimeout((() => {
+            postTeamData(`${season}REG`);
+        }), timeDelay);
         console.log(`${season} Data Added!`);
     });
 }
 
 /**
  * Method which updates team data from a starting season to an ending season in a batch (range -> [startSeason, endSeason])
- * TODO: Find a way to make this more reliable (currently, the connection closes randomly due to the amount of times
- * TODO: it is being called, and likely due to sync issues)
  * @param startSeason
  * @param endSeason
+ * @param timeDelay the amount of time to delay each call by
+ *                  if only updating one season, this can be set to ~100. For every additional season, multiply the time out by between 2-2.5x the previous value
  * @returns {Promise<void>}
  */
-async function batchUpdatePlayerData(startSeason, endSeason) {
-    //Set seasons length
-    let seasons = [];
-    let currSeason = startSeason;
-    for (let i = startSeason; i <= endSeason; i++) {
-        seasons.push(currSeason++);
-    }
-    //Get all team abbreviations
-    seasons.forEach((season) => {
+async function batchUpdatePlayerData(startSeason, endSeason, timeDelay=10000) {
+    //For each season, add player data
+    for (let currSeason = startSeason; currSeason <= endSeason; currSeason++) {
         // Redefining season so that it is specifically regular season data
-        let regSeason = `${season}REG`
+        let regSeason = `${currSeason}REG`
         const fdClientModule = require('fantasydata-node-client');
         const keys = {
             'NFLv3StatsClient': '9a89010dda0643388baf8867abb798df',
@@ -250,43 +265,20 @@ async function batchUpdatePlayerData(startSeason, endSeason) {
                 //You must work with the response (resp) in this callback function. It cannot be used outside the function.
                 let teams = JSON.parse(resp); // <- This is an array of JSON instances. Each instance is a team with their statistics from a given season (see season parameter). There are 32 items in this array cause there are 32 NFL teams.
                 //Update the player stats for each team active this season
-                teams.forEach((team) => {
-                    let abbreviation = team['Key'];
-                    postPlayerData(regSeason, abbreviation).catch((err) => "An error occurred -> " + err);
-                    //setTimeout(postPlayerData(regSeason, abbreviation).catch((err) => "An error occurred -> " + err), 3000);
-                    console.log(`Posted player data for ${abbreviation} ${regSeason}`);
+                teams.forEach((team, index) => {
+                    setTimeout((() => {
+                        let abbreviation = team['Key'];
+                        console.log(abbreviation);
+                        postPlayerData(regSeason, abbreviation).catch((err) => "An error occurred -> " + err);
+                        console.log(`Posted player data for ${abbreviation} ${regSeason}`);
+                    }), (index + 1) * timeDelay);
                 })
             })
             .catch((err) => {
                 console.error("And error has occurred -> " + err)
             });
-    })
-}
-
-/**
- * Method which updates team data from a starting season to an ending season in a batch (range -> [startSeason, endSeason])
- * At the moment, there is no way to do this that doesn't cause an error because of the amount of times
- * we would have to call it to do this insertion
- * @param startSeason
- * @param endSeason
- * @param teamAbbreviation
- * @returns {Promise<void>}
- */
-async function batchUpdatePlayerDataByTeam(startSeason, endSeason, teamAbbreviation) {
-    //Set seasons length
-    let seasons = [];
-    let currSeason = startSeason;
-    for (let i = startSeason; i <= endSeason; i++) {
-        seasons.push(currSeason++);
     }
-    seasons.forEach((season) => {
-        postPlayerData(`${season}REG`, teamAbbreviation);
-        console.log(`${season} Data Added!`);
-    });
 }
-
-//console.log(batchUpdatePlayerDataByTeam(2013, 2022, 'CIN'));
-
 
 /**
  * Removes unnecessary fields from the returned player stats data
@@ -313,3 +305,6 @@ function removeUnneededPlayerData(player) {
     delete player['AverageDraftPosition2QB'];
     delete player['ScoringDetails'];
 }
+
+//Function Testing!
+//batchUpdatePlayerData(2018, 2019, 5000);

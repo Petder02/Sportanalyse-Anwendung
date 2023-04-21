f1d62b06ba24a382b3fb6f07e2684dbe14a563ae
Justin Kocur <64098638+JKGam3r@users.noreply.github.com>
2023-04-21 14:49:01 -0400

    Add files via upload

---
+++ src/HeaderItems.js
@@ -0,0 +1,19 @@
+const headerItems = [
+  {
+    label: 'Dark Mode',
+    slug: 'darkmode',
+    href: 'https://case.edu/',
+  },
+  {
+    label: 'Help',
+    slug: 'help',
+    href: 'https://case.edu/',
+  },
+  {
+    label: 'GitHub',
+    slug: 'github',
+    href: 'https://github.com/Petder02/The-Ronald-Louis',
+  },
+]
+
+export default headerItems
---
+++ src/charts.js
@@ -0,0 +1,15 @@
+import {
+  barchart,
+  linechart,
+} from '@rawgraphs/rawgraphs-charts'
+
+import { calendarHeatmap } from '@rawgraphs/rawgraphs-calendar-heatmap'
+
+// New charts, not included into first release.
+// Comment at necessity.
+let charts = [
+  barchart,
+  linechart
+]
+
+export default charts
